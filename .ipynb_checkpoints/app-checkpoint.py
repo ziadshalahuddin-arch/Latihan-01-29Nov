33488a55e20a64168eba32c56b3d2f58f60465b0
ziad <ziad.shalahuddin@gmail.com>
2025-11-30 23:51:58 +0700

    Initial streamlit multipage app

---
+++ .ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+# Daftar halaman (pages)
+pages = [
+    st.Page(page="pages/page1.py", title="Home", icon="🏠"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="📊"),
+    st.Page(page="pages/page3.py", title="Settings", icon="⚙️")
+]
+
+# Navigasi
+pg = st.navigation(
+    pages,
+    position="sidebar",
+    expanded=True
+)
+
+pg.run()
